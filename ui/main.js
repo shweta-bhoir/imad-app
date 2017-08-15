467e201a10e9fb2f776fdbfec1578474b7e9440b
shweta-bhoir <30933964+shweta-bhoir@users.noreply.github.com>
2017-08-15 16:13:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
-var element = document.getElementbyID('main text');
+var element = document.getElementById('main text');
 element.innerHTML = 'new value';
+var img = document.getElementById('img');
+img.onclick = function()
+{
+    img.style.marginleft = '100px';
+};
