195c266963507cd8b26bb3d1dc2f712418a62912
shweta-bhoir <30933964+shweta-bhoir@users.noreply.github.com>
2017-08-15 14:48:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/article-one',function(req,res)
 });
 app.get('/article-two',function(req,res)
 {
-     res.send(createTemplate(articletwo));
+     res.send(createTemplate(article-two));
     
 });
 app.get('/article-three',function(req,res)
